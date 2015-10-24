d4fe1ff4167904fc2e32b4c38758e20d0c5f0ef6
raul26 <k_vm26@hotmail.com>
2015-10-24 17:00:52 -0500

    all working

--- js/personal.js
+++ js/personal.js
@@ -1,11 +1,33 @@
 $(document).ready(function() {
   $("#little_five").click(function() {
     $('html,body').animate({scrollTop: $("#Cursos").offset().top-90}, 1000);
-    var x = $("#Cursos").offset().top ;
-    console.log(x);
-    console.log(x+90);
+    $('.selected').removeClass('selected');
+    $(this).addClass('selected');
   });
   $("#little_f").click(function() {
     $('html,body').animate({scrollTop: $("#NivelSuperior").offset().top-90}, 1000);
+    $('.selected').removeClass('selected');
+    $(this).addClass('selected');
+  });
+  $("#little_six").click(function() {
+    $('html,body').animate({scrollTop: $("#Recuerda").offset().top-90}, 1000);
+    $('.selected').removeClass('selected');
+    $(this).addClass('selected');
+  });
+
+  $("#cinco").click(function() {
+    $('html,body').animate({scrollTop: $("#Cursos").offset().top-150}, 1000);
+    $('.selected').removeClass('selected');
+    $(this).addClass('selected');
+  });
+  $("#cuatro").click(function() {
+    $('html,body').animate({scrollTop: $("#NivelSuperior").offset().top-150}, 1000);
+    $('.selected').removeClass('selected');
+    $(this).addClass('selected');
+  });
+  $("#seis").click(function() {
+    $('html,body').animate({scrollTop: $("#Recuerda").offset().top-150}, 1000);
+    $('.selected').removeClass('selected');
+    $(this).addClass('selected');
   });
 });
