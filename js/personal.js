5c9d3410d3c6a85276f1f232c7a8f0bfb4bcb848
GabrielVerduzco <verduzcogab@gmail.com>
2015-10-28 19:16:53 -0600

    js

--- js/personal.js
+++ js/personal.js
@@ -101,6 +101,37 @@ function listeners () {
     $('.selected').removeClass('selected');
     $(this).addClass('selected');
   });
+    $("#little_one").click(function() {
+    $('html,body').animate({scrollTop: $("#fondo").offset().top-90}, 1000);
+    $('.selected').removeClass('selected');
+    $(this).addClass('selected');
+  });
+  $("#little_two").click(function() {
+    $('html,body').animate({scrollTop: $("#estudiarLinea").offset().top-90}, 1000);
+    $('.selected').removeClass('selected');
+    $(this).addClass('selected');
+  });
+  $("#little_th").click(function() {
+    $('html,body').animate({scrollTop: $("#bachillerato").offset().top-90}, 1000);
+    $('.selected').removeClass('selected');
+    $(this).addClass('selected');
+  });
+
+  $("#uno").click(function() {
+    $('html,body').animate({scrollTop: $("#fondo").offset().top-150}, 1000);
+    $('.selected').removeClass('selected');
+    $(this).addClass('selected');
+  });
+  $("#dos").click(function() {
+    $('html,body').animate({scrollTop: $("#estudiarLinea").offset().top-150}, 1000);
+    $('.selected').removeClass('selected');
+    $(this).addClass('selected');
+  });
+  $("#tres").click(function() {
+    $('html,body').animate({scrollTop: $("#bachillerato").offset().top-150}, 1000);
+    $('.selected').removeClass('selected');
+    $(this).addClass('selected');
+  });
 
   $("#cinco").click(function() {
     $('html,body').animate({scrollTop: $("#Cursos").offset().top-150}, 1000);
